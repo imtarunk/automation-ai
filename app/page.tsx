6839d84c45ee9a5ebd8887e1fc84f1a8ff81da2d
imtarunk <tarunshr145@gmail.com>
2025-04-12 21:36:38 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import { ContainerScroll } from "@/components/global/container-scroll-animation";
+import Navbar from "@/components/global/navbar";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased">
+        <div className="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll
+            titleComponent={
+              <div className="flex items-center justify-center flex-col">
+                <Button
+                  size={"lg"}
+                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#F1F1F1] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                />
+              </div>
+            }
+          />
+        </div>
+      </section>
+    </main>
+  );
+}
